feeb018f303c96d67f2ce3dfb84f5b80466f1f75
Haedara Salloum <haedarahasan69@gmail.com>
2026-07-06 12:32:17 +0300

    updates

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // <- أضف هذا السطر
+
+  // اختياري: إذا كنت تريد مسارات مثل /about/ بدلاً من /about.html
+  // trailingSlash: true,
+
+  // اختياري: تغيير مجلد الإخراج من 'out' إلى اسم آخر
+  // distDir: 'dist',
 };
 
 export default nextConfig;
